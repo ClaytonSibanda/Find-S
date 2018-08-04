d20be8088018f113dc85a5c48ae194fd37a35d75
Clayton <sibandaclayton0@gmail.com>
2018-08-04 17:45:20 +0200

    first commit

---
+++ cmake-build-debug/CMakeFiles/Find_S.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Find_S.dir/main.cpp.obj"
+  "Find_S.pdb"
+  "Find_S.exe"
+  "Find_S.exe.manifest"
+  "libFind_S.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Find_S.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Find_S.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Find_S.
+# This may be replaced when dependencies are built.
